c6ba7719efb34b3a5c4f3ede60cb88d8a4ccfadb
Hazel Wright <hazel.wright@live.co.uk>
2020-01-18 00:35:56 +0000

    now with more spam

--- git-tip-bot/__init__.py
+++ git-tip-bot/__init__.py
@@ -1,4 +1,7 @@
+import csv
+import json
 import os
+import random
 
 from slack import WebClient
 
@@ -7,12 +10,28 @@ def start():
     token = os.getenv('GIT_TIP_BOT_TOKEN')
     client = WebClient(token)
 
-    # TODO: how do we send a message to every user that has added the bot
-    # to their apps?
-    response = client.chat_postMessage(
-        channel='#git-tip-bot',
-        text='hi from bot'
-    )
+    with open('tips.csv') as tips_file:
+        tips_reader = csv.reader(tips_file, delimiter='|')
+        tips = [tip for (tip,) in tips_reader]
+
+    tip = random.choice(tips)
+
+    while True:
+        cursor = ''
+        response = client.users_list(cursor=cursor).data
+        members = response['members']
+        user_ids = [member['id'] for member in members]
+
+        for user_id in user_ids:
+             client.chat_postMessage(
+                channel=user_id,
+                text=tip
+            )
+
+        cursor = response['response_metadata']['next_cursor']
+
+        if not cursor:
+            break
 
 
 start()
